1fe396b5f3f8ebf64206e9b57da1b44e1f033890
AprigioCavalcanti <aprigiocavalcanti@gmail.com>
2026-08-12 13:30:21 -0300

    correções e comentários

--- Atividade 11-08-2026/extrairDisciplinas.js
+++ Atividade 11-08-2026/extrairDisciplinas.js
@@ -3,17 +3,26 @@ var rows = contentBoxBody.querySelectorAll('tr');
 
 var disciplinas = [];
 
-for (var i = 1; i < rows.length; i++) {
-    var row = rows[i];
-    var columns = row.querySelectorAll('td');
-
-    var nomeCodigo = columns[0].textContent.trim();
-    var codigo = nomeCodigo.split(" ")[0];
-
-    disciplinas.push({
-        codigo: codigo,
-        nome: nomeCodigo.substring(codigo.length).trim()
-    });
+for (var i = 1; i < rows.length; i++) {//ignorando i=0 pois este é cabeçalho
+	var row = rows[i];
+    var columns = row.querySelectorAll("td"); //as colunas são: codigo+nome, periodo, atendida, tipo, ramificação, cred, ch, trava e turma no periodo
+	var nomeCodigo = columns[0].textContent.trim();
+	var codigo = nomeCodigo.split(" ")[0];
+	var nome = nomeCodigo.replace(codigo + " ", "");
+	var periodo = columns[1].textContent.trim();
+	var tipo = columns[3].textContent.trim();
+	var cred = columns[5].textContent.trim();
+	var ch = columns[6].textContent.trim();
+	var travaCredito = columns[7].textContent.trim();
+	disciplinas.push({
+		"nome" : nome, 
+		"codigo" : codigo, 
+		"periodo" : periodo, 
+		"tipo" : tipo, 
+		"creditos" : cred, 
+		"carga_horaria" : ch, 
+		"trava_credito" : travaCredito
+	});
 }
 
 console.log(disciplinas);
